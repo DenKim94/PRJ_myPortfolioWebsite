61e0896a7188e9bd40dbdd264d6a4991fb3ce17f
Denis Kim <denis-kim@outlook.de>
2024-10-13 16:58:33 +0200

    started with portfolio section style props

--- my-website-app/src/components/PortfolioSection.jsx
+++ my-website-app/src/components/PortfolioSection.jsx
@@ -1,10 +1,26 @@
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
+import './../styles/SectionsGeneric.css'
+import './../styles/PortfolioSection.css'
+
 function PortfolioSection() {
-    return ( 
-        <section id='portfolio'className='portfolio-section'>
-            <h1>Meine Projekte</h1>
-            <p>Welcome to the Project section.</p>
-        </section>
-        );
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  return (
+    <motion.section
+      ref={ref}
+      initial={{ y: '100%' }}
+      animate={isInView ? { y: 0 } : { y: '100%' }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+      id='portfolio'
+      className='portfolio-section'
+    >
+      <h1>Meine Projekte</h1>
+      <p>Welcome to the Project section.</p>
+    </motion.section>
+  );
 }
 
 export default PortfolioSection;
